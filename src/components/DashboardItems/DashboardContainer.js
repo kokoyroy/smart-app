1bf6462d0bc1ab1e2f1d94b143b1d9bc6eb4c4bd
kokoyroy <kokoyroy@gmail.com>
2021-01-23 00:54:35 +0200

    commit 5?

--- src/components/DashboardItems/DashboardContainer.js
+++ src/components/DashboardItems/DashboardContainer.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import style from './DashboardContainer.module.css';
+import Dashboarditem from './Dashboarditem';
 function DashboardContainer() {
     return (
-        <div class={`${style.dashboardcontainer}`}>
-            
-        </div>
+        <div className={`${style.dashboardcontainer}`}>
+            <div className='row'>
+                <div className="col s4"><Dashboarditem /></div>
+                <div className="col s4"><Dashboarditem /></div>
+                <div className="col s4"><Dashboarditem /></div>
+                <div className="col s4"><Dashboarditem /></div>
+                <div className="col s4"><Dashboarditem /></div>
+                <div className="col s4"><Dashboarditem /></div>s 
+
+            </div>
+            <div className='row'>
+
+            </div>
+        </ div>
     )
 }
 
--- src/components/DashboardItems/Dashboarditem.js
+++ src/components/DashboardItems/Dashboarditem.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import style from './Dashboarditem.module.css';
+import food from '../../assets/lily-banse--YHSwy6uqvk-unsplash.jpg'
 function Dashboarditem() {
     return (
-        <div>
-            
+
+        <div className={`${style.item} card `}>
+            <div className="card-image waves-effect waves-block waves-light">
+                <img className="activator" src={food} alt='food img' />
+            </div>
+            <div className="card-content">
+                <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
+                <p><a href="/">This is a link</a></p>
+            </div>
+            <div className="card-reveal">
+                <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
         </div>
+
+
     )
 }
 
