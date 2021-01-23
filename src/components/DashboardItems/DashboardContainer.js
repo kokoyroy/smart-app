8101b94ee3b665176ca69c07fd00e3b0c5b224c2
kokoyroy <kokoyroy@gmail.com>
2021-01-23 04:51:50 +0200

    consumed the api for sidebar and foods, food grid destroyed...

--- src/components/DashboardItems/DashboardContainer.js
+++ src/components/DashboardItems/DashboardContainer.js
@@ -6,54 +6,24 @@ import Dashboarditem from './Dashboarditem';
 
 
 function DashboardContainer(props) {
-   const [items, setitems] = useState(null)
-    
-   useEffect(() => {
-       fetch(`https://us-central1-smartup-hr-test-frontend.cloudfunctions.net/categories/${props.foodType}`)
-       .then(res=>res.json()).then(res=>setitems(res.items))
-   },[props.foodType])
-    
+    const [items, setitems] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://us-central1-smartup-hr-test-frontend.cloudfunctions.net/categories/${props.foodType}`)
+            .then(res => res.json()).then(res => setitems(res.items))
+    }, [props.foodType])
 
 
-    if(items)console.log(items);
+
+    if (items) console.log(items);
     return (
         <div className={`${style.dashboardcontainer}`}>
             <div className='row'>
-
-
-            {items?items.map(item=>(
-                <div key={item.id} className='col s4'><Dashboarditem   {...item}/></div>
-            )) :<div>loading</div>}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* 
-                <div className="col s4"> foodType={props.foodType}/></div>
-                <div className="col s4"><Dashboarditem foodType={props.foodType}/></div>
-                <div className="col s4"><Dashboarditem foodType={props.foodType}/></div>
-                <div className="col s4"><Dashboarditem foodType={props.foodType}/></div>
-                <div className="col s4"><Dashboarditem foodType={props.foodType}/></div>
-                <div className="col s4"><Dashboarditem foodType={props.foodType}/></div> */}
-
+                {items ? items.map(item => (
+                    <div key={item.id} className='col s4'><Dashboarditem   {...item} /></div>
+                )) : <div>loading</div>}
             </div>
-            <div className='row'>
 
-            </div>
         </ div>
     )
 }
